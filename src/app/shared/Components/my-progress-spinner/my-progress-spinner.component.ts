2eacd3f31342bfa304f1842753a2ad9a4a1ee79d
kinwend17 <kinwend17@kinwend-pc>
2022-12-07 01:03:58 +0300

    add spinner and fonts

--- src/app/shared/Components/my-progress-spinner/my-progress-spinner.component.ts
+++ src/app/shared/Components/my-progress-spinner/my-progress-spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-progress-spinner',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-progress-spinner.component.scss']
 })
 export class MyProgressSpinnerComponent implements OnInit {
+
+  @Input() sizePx: number = 50;
 
   constructor() { }
 
