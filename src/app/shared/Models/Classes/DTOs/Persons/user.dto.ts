f3c26510815253e3972eaf1496104067e609e027
kinwend17 <kinwend17@kinwend-pc>
2022-12-07 06:06:14 +0300

    some

--- src/app/shared/Models/Classes/DTOs/Persons/user.dto.ts
+++ src/app/shared/Models/Classes/DTOs/Persons/user.dto.ts
@@ -3,7 +3,6 @@ import { AuditablePersonDTO } from '../base/auditable.dto';
 export class UserDto extends AuditablePersonDTO {
   public email: string = '';
   public password: string = '';
-  public middleName: string = '';
   public gender: string = '';
   public role: string = '';
   public phone: string = '';
