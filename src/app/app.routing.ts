47d1844d3555509bc108fecc0cbf31b44ac8df9e
Andrey Starostenkov <st.andrey17@yandex.ru>
2023-01-08 19:39:59 +0300

    add routes to profile

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
   {
     path: 'profile',
     loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule),
-    pathMatch: 'full',
+    // pathMatch: 'full',
     canActivate: [AuthGuard],
   },
   {
--- src/app/pages/profile/profile.routing.ts
+++ src/app/pages/profile/profile.routing.ts
@@ -1,10 +1,27 @@
 import { RouterModule, Routes } from "@angular/router";
 import { ProfilePage } from './profile.page';
+import { OrderInfoComponent } from './order-info/order-info.component';
+import { ProfileInfoComponent } from './profile-info/profile-info.component';
+import { AuthGuard } from '../../shared/Guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: ProfilePage,
+    children: [
+      {
+        path: '',
+        component: ProfileInfoComponent,
+        pathMatch: 'full',
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'orders',
+        component: OrderInfoComponent,
+        pathMatch: 'full',
+        canActivate: [AuthGuard]
+      }
+    ],
   },
 ];
 
