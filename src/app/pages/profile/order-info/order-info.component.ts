94df14eadf6384a27331e77b906179d55d9e9903
Andrey Starostenkov <st.andrey17@yandex.ru>
2023-01-08 20:01:07 +0300

    пользователи теперь могут давать согласие/отказ в списке своих заказов

--- src/app/pages/profile/order-info/order-info.component.ts
+++ src/app/pages/profile/order-info/order-info.component.ts
@@ -2,11 +2,12 @@ import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { OrderDTO } from '../../../shared/Models/Classes/DTOs/order.dto';
 import { OfferDTO } from '../../../shared/Models/Classes/DTOs/offer.dto';
 import { KeyValueDTO } from '../../../shared/Models/Classes/DTOs/base/key-value.dto';
-import { BehaviorSubject, Observable, combineLatest, map, shareReplay } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest, map, shareReplay, switchMap, firstValueFrom } from 'rxjs';
 import { OfferService } from '../../../shared/Services/offer/offer.service';
 import { OrderService } from '../../../shared/Services/order/order.service';
 import { MyMessageService } from 'src/app/shared/Services/my-message.service';
 import { AuthService } from 'src/app/shared/Services/auth/auth.service';
+import { OrderForm } from '../../../shared/Models/Classes/Forms/order.form';
 
 @Component({
   selector: 'app-order-info',
@@ -20,8 +21,8 @@ export class OrderInfoComponent implements OnInit {
   private _refresh$ = new BehaviorSubject<boolean>(false);
   private _needRefresh$ = this._refresh$.asObservable();
 
-  orders$: Observable<OrderDTO[]> = combineLatest([this.srvOrder.getAllByUserId(this.auth.userId), this._needRefresh$]).pipe(
-    map(data => data[0]),
+  orders$: Observable<OrderDTO[]> = this._needRefresh$.pipe(
+    switchMap(() => this.srvOrder.getAllByUserId(this.auth.userId)),
     shareReplay()
   );
 
@@ -35,5 +36,22 @@ export class OrderInfoComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+  }
+
+  onLog(ev: any) {
+    console.log(ev);
+  }
+
+  onChangeAgreement(item: OrderDTO) {
+    const form: OrderForm = new OrderForm(item.offer.id, item.user.id, item.orderCost, item.orderDate, !item.agreement);
+
+    firstValueFrom(this.srvOrder.update(item.id, form))
+    .then(() => {
+      this.srvMsg.showSuccess('Данные обновлены!');
+      this._refresh$.next(true);
+    })
+    .catch((error) => {
+      this.srvMsg.showException(error);
+    });
   }
 }
